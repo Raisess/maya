32d87cce130e9ec3de378dca4dd7087f16a3b589
Raisess <raisechif@gmail.com>
2021-03-24 12:01:58 -0300

    created is colliding phisic method

--- src/engine/Phisics.ts
+++ src/engine/Phisics.ts
@@ -15,5 +15,21 @@ export default class Phisics {
 			}
 		}, 20);
 	}
+
+	public static isColliding(entityX: IEntity, entityY: IEntity): boolean {
+		const left:   [number, number] = [entityX.getPosX(), entityY.getPosX()];
+		const right:  [number, number] = [entityX.getPosX() + entityX.getWidth(), entityY.getPosX() + entityY.getWidth()];
+		const top:    [number, number] = [entityX.getPosY(), entityY.getPosY()];
+		const bottom: [number, number] = [entityX.getPosY() + entityX.getHeight(), entityY.getPosY() + entityY.getHeight()];
+
+		if ((bottom[0] < top[1]) ||
+		   (top[0] > bottom[1])  ||
+		   (right[0] < left[1])  ||
+		   (left[0] > right[1])) {
+			return false;
+		}
+
+		return true;
+	}
 }
 
