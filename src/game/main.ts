8a37d8f2499903be7e718698fa826a9a993833e1
Raisess <raisechif@gmail.com>
2021-03-26 09:59:31 -0300

    updated engine implementation

--- src/game/main.ts
+++ src/game/main.ts
@@ -65,7 +65,7 @@ const i: any = Utils.loop((): void => {
 
 let acc: number = 0;
 
-scene.onKeyboardEvent((ev: KeyboardEvent): void => {
+Utils.onKeyboardEvent((ev: KeyboardEvent): void => {
 	switch (ev.key) {
 		case "w": player.setPosY(player.getPosY() - 100);
 			break;
@@ -73,34 +73,37 @@ scene.onKeyboardEvent((ev: KeyboardEvent): void => {
 			break;
 		case "a": player.setPosX(player.getPosX() - 10);
 			break;
-		case "k":
-			if (acc < 1) {
-				acc++;
-				const power: Entity = new Entity(
-					"power",
-					"blue",
-					{ width: 30, height: 40 },
-					{ x: -100, y: -100 },
-					"solid"
-				);
-
-				scene.addEntity(power);
-				power.setPosX(player.getPosX());
-				power.setPosY(player.getPosY());
-
-				const a: unknown = Utils.loop((): void => {
-					power.setPosX(power.getPosX() + 10);
-
-					if (power.getPosX() > 1080) {
-						scene.destroyEntity(power);
-						Utils.clearLoop(a);
-						acc--;
-					}
-				});
-			}
-			break;
 		default:
 			break;
+	}
+
+	if (ev.key === "k" || ev.key === "j") {
+		if (acc < 3) {
+			acc++;
+			const power: Entity = new Entity(
+				"power",
+				ev.key === "k" ? "blue" : "yellow",
+				{ width: 30, height: 40 },
+				{ x: -100, y: -100 },
+				"solid"
+			);
+
+			scene.addEntity(power);
+			power.setPosX(player.getPosX());
+			power.setPosY(player.getPosY());
+
+			const a: unknown = Utils.loop((): void => {
+				power.setPosX(power.getPosX() + 10);
+
+				if (power.getPosX() > (player.getPosX() + 300) || Phisics.isColliding(power, block)) {
+					scene.destroyEntity(power);
+					Utils.clearLoop(a);
+					acc--;
+				}
+
+				if (Phisics.isColliding(power, block)) alert("a");
+			});
+		}
 	}
 });
 
